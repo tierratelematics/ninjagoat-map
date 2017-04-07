111bbb08c887491b39ff36b66af4a5b557c5ef07
Mattia Franchetto <mt.franchetto@gmail.com>
2017-04-07 16:59:10 +0200

    Implement bounding box changed spec

--- test/LayerPresenterSpec.ts
+++ test/LayerPresenterSpec.ts
@@ -4,34 +4,38 @@ import {IMock, Mock, Times, It} from "typemoq";
 import ILayerPresenter from "../scripts/interfaces/ILayerPresenter";
 import LayerPresenter from "../scripts/LayerPresenter";
 import ILayerView from "../scripts/interfaces/ILayerView";
-import {Subject} from "rx";
+import {ReplaySubject} from "rx";
 
 describe("Given a layer presenter", () => {
 
     let subject: ILayerPresenter;
     let geojsonView: IMock<ILayerView<any, any>>;
-    let data: Subject<any>;
+    let data: ReplaySubject<any>;
 
     beforeEach(() => {
-        data = new Subject<any>();
+        data = new ReplaySubject<any>();
         geojsonView = Mock.ofType<ILayerView<any, any>>();
         subject = new LayerPresenter({"GeoJSON": geojsonView.object});
     });
 
     context("when a layer is shown for the first time", () => {
+        beforeEach(() => {
+            data.onNext({markers: []});
+        });
         it("should be created", () => {
             subject.present(context => data, "GeoJSON");
-            data.onNext({markers: []});
 
             geojsonView.verify(g => g.create(It.isValue({markers: []}), null), Times.once());
         });
     });
 
     context("when the data gets an update", () => {
-        it("the layer itself should be updated", () => {
-            subject.present(context => data, "GeoJSON");
+        beforeEach(() => {
             data.onNext({markers: []});
             data.onNext({markers: [{id: "8283"}]});
+        });
+        it("the layer itself should be updated", () => {
+            subject.present(context => data, "GeoJSON");
 
             geojsonView.verify(g => g.update(It.isValue({markers: []}), It.isValue({markers: [{id: "8283"}]}), null), Times.once());
         });
@@ -48,6 +52,18 @@ describe("Given a layer presenter", () => {
     });
 
     context("when the bounding box changes", () => {
-        it("should reload the source with the new bounding box");
+        beforeEach(() => {
+            data.onNext({markers: []});
+            data.onNext({markers: [{id: "8283"}]});
+        });
+        it("should reload the source with the new bounding box", () => {
+            let layer = subject.present(context => {
+                if (context.zoom) data.onNext({zoom: context.zoom});
+                return data;
+            }, "GeoJSON");
+            layer.refresh({bounds: null, zoom: 18});
+
+            geojsonView.verify(g => g.update(It.isValue({markers: [{id: "8283"}]}), It.isValue({zoom: 18}), null), Times.once());
+        });
     });
 });
