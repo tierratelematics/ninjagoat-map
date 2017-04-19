c0b9a5e3dd186b22010d69ca56584d7605bb83a5
Mattia Franchetto <mt.franchetto@gmail.com>
2017-04-19 17:30:06 +0200

    Add trivial management for geojson update

--- scripts/geojson/GeoJSONLayerView.ts
+++ scripts/geojson/GeoJSONLayerView.ts
@@ -6,14 +6,16 @@ import {injectable} from "inversify";
 
 @injectable()
 class GeoJSONLayerView implements ILayerView<GeoJSONProps, void> {
-    type:LayerType = "GeoJSON";
+    type: LayerType = "GeoJSON";
 
     create(props: GeoJSONProps, options: void): Layer {
         return geoJSONLayer(props);
     }
 
     update(fromProps: GeoJSONProps, toProps: GeoJSONProps, layer: Layer, options: void) {
-
+        let untypedLayer = (<any>layer); //Due since leaflet types are incomplete
+        untypedLayer.clearLayers();
+        untypedLayer.addData(toProps);
     }
 
 }
