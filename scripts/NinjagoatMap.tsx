2a85ca2ce356d1e508d465e9d767855cc980334e
Mattia Franchetto <mt.franchetto@gmail.com>
2017-04-27 14:23:32 +0200

    Change default map boundaries

--- scripts/NinjagoatMap.tsx
+++ scripts/NinjagoatMap.tsx
@@ -9,7 +9,7 @@ class NinjagoatMap extends React.Component<void, void> {
     private mapHolder: IMapHolder;
 
     render() {
-        return <Map center={[45, 45]} zoom={8} ref={component => this.mapHolder.setMap(component.leafletElement)}>
+        return <Map center={[0, 0]} zoom={1} ref={component => this.mapHolder.setMap(component.leafletElement)}>
             {this.props.children}
         </Map>;
     }
