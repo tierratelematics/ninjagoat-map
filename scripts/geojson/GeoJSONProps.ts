92b7ff09096bc399d3c745f05109f9e6e342d426
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-02 17:18:39 +0200

    Fix geojson types

--- scripts/geojson/GeoJSONProps.ts
+++ scripts/geojson/GeoJSONProps.ts
@@ -1,6 +1,6 @@
 import {GeoJSONOptions} from "leaflet";
 import {MapObservableFactory} from "../layer/MapContext";
 
-export type GeoJSONCollection = GeoJSON.FeatureCollection<GeoJSONCollection.GeometryObject>;
+export type GeoJSONCollection = GeoJSON.FeatureCollection<GeoJSON.GeometryObject>;
 
 export type GeoJSONProps = GeoJSONOptions & {observable: MapObservableFactory<GeoJSONCollection>};
