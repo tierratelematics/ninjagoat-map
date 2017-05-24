1279611704bf17b3de5019b6f9fb6080fc8d2ebd
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-24 12:17:58 +0200

    Export latLng and latLngBounds

--- scripts/ninjagoat-map.ts
+++ scripts/ninjagoat-map.ts
@@ -3,7 +3,7 @@ export {default as MapModule} from "./MapModule";
 export {Map} from "./Map";
 export {default as GeoJSONLayer} from "./geojson/GeoJSONLayer";
 export {TileLayer, WMSTileLayer} from "react-leaflet";
-export {default as CoordinatesUtil} from "./leaflet/CoordinatesUtil";
 export {DrawingLayer} from "./draw/DrawingLayer";
 export {EditControl as DrawControl} from "react-leaflet-draw";
 export {GeocodingControl} from "./geocoding/GeocodingControl";
+export {LatLng, LatLngBounds, latLng, latLngBounds} from "leaflet";
