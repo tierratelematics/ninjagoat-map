824e63a5dd076cc5fc76f64d508d86f8155fa540
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-25 15:20:33 +0200

    Prevent popup caching

--- scripts/popup/PopupLayer.tsx
+++ scripts/popup/PopupLayer.tsx
@@ -2,13 +2,19 @@ import * as React from "react";
 import {Popup} from "react-leaflet";
 import PopupProps from "./PopupProps";
 import {LatLngExpression} from "leaflet";
+import {GeoJSONFeature} from "../geojson/GeoJSONProps";
+import {lazyInject, IGUIDGenerator} from "ninjagoat";
 
 class PopupLayer extends React.Component<PopupProps, void> {
+
+    @lazyInject("IGUIDGenerator")
+    private guidGenerator: IGUIDGenerator; //Used to recreate the popup every time the feature is updated
 
     render() {
         return !this.props.feature ? null :
             <Popup position={this.props.feature.geometry.coordinates as LatLngExpression}
                    offset={{x: 0, y: -28} as any}
+                   key={this.guidGenerator.generate()}
                    onpopupclose={this.props.onPopupClose}>{this.props.children}</Popup>;
     }
 }
