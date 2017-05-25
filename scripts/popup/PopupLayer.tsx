c691f8480d96722b8b9d2bd4fb1b985a83aa2724
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-25 17:05:00 +0200

    Fix coordinates for popup

--- scripts/popup/PopupLayer.tsx
+++ scripts/popup/PopupLayer.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import {Popup} from "react-leaflet";
 import PopupProps from "./PopupProps";
-import {LatLngExpression} from "leaflet";
-import {GeoJSONFeature} from "../geojson/GeoJSONProps";
+import {LatLngExpression, latLng} from "leaflet";
 import {lazyInject, IGUIDGenerator} from "ninjagoat";
 
 class PopupLayer extends React.Component<PopupProps, void> {
@@ -11,11 +10,14 @@ class PopupLayer extends React.Component<PopupProps, void> {
     private guidGenerator: IGUIDGenerator; //Used to recreate the popup every time the feature is updated
 
     render() {
-        return !this.props.feature ? null :
-            <Popup position={this.props.feature.geometry.coordinates as LatLngExpression}
-                   offset={{x: 0, y: -28} as any}
-                   key={this.guidGenerator.generate()}
-                   onpopupclose={this.props.onPopupClose}>{this.props.children}</Popup>;
+        if (!this.props.feature)
+            return null;
+        let coordinates = this.props.feature.geometry.coordinates as number[];
+        return <Popup
+            position={latLng(coordinates[1], coordinates[0])}
+            offset={{x: 0, y: -28} as any}
+            key={this.guidGenerator.generate()}
+            onpopupclose={this.props.onPopupClose}>{this.props.children}</Popup>;
     }
 }
 
