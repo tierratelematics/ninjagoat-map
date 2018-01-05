e5a606604ac3f1a62321256084ec34055446677e
Mattia Franchetto <mt.franchetto@gmail.com>
2018-01-05 12:15:25 +0100

    Add map click handler

--- scripts/Map.tsx
+++ scripts/Map.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import {Map as LeafletMap} from "react-leaflet";
 import {lazyInject} from "ninjagoat";
 import IMapHolder from "./leaflet/IMapHolder";
-import {MapOptions} from "leaflet";
+import {MapOptions, Event} from "leaflet";
 
 export type MapProps = MapOptions & {
-    onMapReady?: () => void
+    onMapReady?: () => void,
+    onMapClick?(event: Event): void; 
 }
 
 export class Map extends React.Component<MapProps, void> {
@@ -19,6 +20,7 @@ export class Map extends React.Component<MapProps, void> {
             if (component && !map) {
                 this.mapHolder.setMap(component.leafletElement);
                 if (this.props.onMapReady) this.props.onMapReady();
+                map.on("click", this.props.onMapClick);
             }
         }}>
             {this.props.children}
@@ -26,6 +28,10 @@ export class Map extends React.Component<MapProps, void> {
     }
 
     componentWillUnmount() {
-        this.mapHolder.setMap(null);
+        let map = this.mapHolder.obtainMap();
+        if (!map) {
+            map.on("click", this.props.onMapClick);
+            this.mapHolder.setMap(null);
+        }
     }
 }
