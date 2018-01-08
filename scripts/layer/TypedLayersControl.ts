a9fe7ef5a554acadf3087554b0e79bab4f27dd4b
Mattia Franchetto <mt.franchetto@gmail.com>
2018-01-08 11:35:00 +0100

    Add typed layers control

---
+++ scripts/layer/TypedLayersControl.ts
@@ -0,0 +1,45 @@
+
+import { LayersControl } from "react-leaflet";
+
+export class TypedLayersControl<P> extends LayersControl implements React.Component<P, any> {
+    
+    render(): false | JSX.Element {
+        return super.render();
+    }
+
+    setState<K extends string>(f: (prevState: any, props: P) => Pick<any, K>, callback?: () => void): void;
+    setState<K extends string>(state: Pick<any, K>, callback?: () => void): void;
+    setState(state: any, callback?: any) {
+        super.setState(state, callback);
+    }
+    forceUpdate(callBack?: () => void): void {
+        super.forceUpdate(callBack);
+    }
+    props: Readonly<{ children?: React.ReactNode; }> & Readonly<P>;
+    state: Readonly<any>;
+    context: any;
+    refs: { [key: string]: React.ReactInstance; };
+
+    componentWillMount?(): void {
+        super.componentWillMount();
+    }
+    componentDidMount?(): void {
+        super.componentDidMount();
+    }
+    componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void {
+        super.componentWillReceiveProps(nextProps, nextContext);
+    }
+    shouldComponentUpdate?(nextProps: Readonly<P>, nextState: Readonly<any>, nextContext: any): boolean {
+        return super.shouldComponentUpdate(nextProps, nextState, nextContext);
+    }
+    componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<any>, nextContext: any): void {
+        super.componentWillUpdate(nextProps, nextState, nextContext);
+    }
+    componentDidUpdate?(prevProps: Readonly<P>, prevState: Readonly<any>, prevContext: any): void {
+        super.componentDidUpdate(prevProps, prevState, prevContext);
+    }
+    componentWillUnmount?(): void {
+        super.componentWillUnmount();
+    }
+
+}
--- scripts/leaflet/MapControl.ts
+++ scripts/leaflet/MapControl.ts
@@ -1,7 +1,7 @@
-export const {LayersControl} = require("react-leaflet");
+import { TypedLayersControl } from "../layer/TypedLayersControl";
 const PropTypes = require("prop-types");
 
-abstract class MapControl<P> extends LayersControl<P> {
+abstract class MapControl<P> extends TypedLayersControl<P> {
 
     // Patch to link context correctly in React (probably due since static fields are not inherited from javascript classes)
     static contextTypes = {
