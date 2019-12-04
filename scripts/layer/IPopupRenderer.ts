0601cd636e417ac6f11875fee010773c7e33bd1c
Enrico Calzavara <enrico.calzavara@gmail.com>
2019-12-04 14:30:27 +0100

    Handle content's optionality and pass popup element in onPopupRendered callback

--- scripts/layer/IPopupRenderer.ts
+++ scripts/layer/IPopupRenderer.ts
@@ -2,5 +2,5 @@ import { Layer } from "leaflet";
 import { PopupContext } from "../geojson/GeoJSONProps";
 
 export interface IPopupRenderer {
-    renderOn(layer: Layer, context: PopupContext): void;
+    renderOn(layer: Layer, context: PopupContext): HTMLElement;
 }
